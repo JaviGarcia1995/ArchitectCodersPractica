54f9dfc88e1ae7a2a169f0a90305f86d73dec259
LeSamBoi <fco.javier.garcia7@gmail.com>
2024-04-21 17:41:48 +0200

    [Del] Removed useless functions

--- app/src/main/java/com/example/architectcoderspracticauno/MainActivity.kt
+++ app/src/main/java/com/example/architectcoderspracticauno/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.architectcoderspracticauno
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,9 +10,15 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.architectcoderspracticauno.data.repository.HogwartsRepository
 import com.example.architectcoderspracticauno.ui.theme.ArchitectCodersPracticaUnoTheme
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,25 +30,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ArchitectCodersPracticaUnoTheme {
-        Greeting("Android")
     }
 }
