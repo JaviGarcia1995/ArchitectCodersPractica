e83ed942e0fa1eabd15527226eaf7498e9099d35
LeSamBoi <fco.javier.garcia7@gmail.com>
2024-04-21 17:42:37 +0200

    [Del] Removed unused imports

--- app/src/main/java/com/example/architectcoderspracticauno/MainActivity.kt
+++ app/src/main/java/com/example/architectcoderspracticauno/MainActivity.kt
@@ -1,24 +1,13 @@
 package com.example.architectcoderspracticauno
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.architectcoderspracticauno.data.repository.HogwartsRepository
 import com.example.architectcoderspracticauno.ui.theme.ArchitectCodersPracticaUnoTheme
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
