b4730e88168c2d633541aecbd7409c19ac2e77df
LeSamBoi <fco.javier.garcia7@gmail.com>
2024-04-22 12:23:45 +0200

    [Add] New Screen function for all activities

--- app/src/main/java/com/example/architectcoderspracticauno/MainActivity.kt
+++ app/src/main/java/com/example/architectcoderspracticauno/MainActivity.kt
@@ -7,21 +7,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import com.example.architectcoderspracticauno.ui.screens.home.HomeScreen
 import com.example.architectcoderspracticauno.ui.theme.ArchitectCodersPracticaUnoTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ArchitectCodersPracticaUnoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-
-                }
-            }
+            HomeScreen()
         }
     }
 }
