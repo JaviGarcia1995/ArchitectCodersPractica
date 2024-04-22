2c2a1e12982e46684eef73961a3f012f0dba38b8
LeSamBoi <fco.javier.garcia7@gmail.com>
2024-04-22 12:24:30 +0200

    [Add] Load images from repository and also added MVVM for HomeScreen

---
+++ app/src/main/java/com/example/architectcoderspracticauno/ui/screens/home/HomeViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.architectcoderspracticauno.ui.screens.home
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.architectcoderspracticauno.data.model.Wizard
+import com.example.architectcoderspracticauno.data.repository.HogwartsRepository
+import kotlinx.coroutines.launch
+
+class HomeViewModel: ViewModel() {
+    var state by mutableStateOf(UiState())
+        private set
+
+    private val repository = HogwartsRepository()
+
+    fun onUiReady(house: String){
+        viewModelScope.launch {
+            state = UiState(loading = true)
+            state = UiState(loading = false, repository.getWizardsSortedByHouse(house))
+        }
+    }
+
+    data class UiState(
+        val loading: Boolean = false,
+        val wizards: List<Wizard> = emptyList()
+    )
+}
