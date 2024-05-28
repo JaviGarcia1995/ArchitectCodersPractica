d38f8c9ee4e0ba39e090aa63a8248753587fa936
LeSamBoi <fco.javier.garcia7@gmail.com>
2024-05-28 17:46:25 +0200

    [ADD] Slide animations to Navigation between HomeScreen and Detail Screen

--- app/src/main/java/com/example/architectcoderspracticauno/ui/screens/navigation/Navigation.kt
+++ app/src/main/java/com/example/architectcoderspracticauno/ui/screens/navigation/Navigation.kt
@@ -1,5 +1,7 @@
 package com.example.architectcoderspracticauno.ui.screens.navigation
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.tween
 import androidx.compose.runtime.Composable
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
@@ -13,17 +15,24 @@ import com.example.architectcoderspracticauno.ui.screens.home.HomeScreen
 @Composable
 fun Navigation() {
     val navController = rememberNavController()
+    val animationDuration = 800
 
     NavHost(
         navController = navController,
         startDestination = Home
     ){
-        composable<Home>{
+        composable<Home>(
+            enterTransition = { slideIntoContainer(AnimatedContentTransitionScope.SlideDirection.End, tween(animationDuration)) },
+            exitTransition = { slideOutOfContainer(AnimatedContentTransitionScope.SlideDirection.Start, tween(animationDuration)) },
+        ){
             HomeScreen( onWizardClicked = { wizard ->
                 navController.navigate(Detail(wizard.id))
             })
         }
-        composable<Detail>{backStackEntry ->
+        composable<Detail>(
+            enterTransition = { slideIntoContainer(AnimatedContentTransitionScope.SlideDirection.Start, tween(animationDuration)) },
+            exitTransition = { slideOutOfContainer(AnimatedContentTransitionScope.SlideDirection.End, tween(animationDuration)) },
+        ){backStackEntry ->
             val detail = backStackEntry.toRoute<Detail>()
             DetailScreen(
                 viewModel { DetailViewModel(detail.wizardId) },
